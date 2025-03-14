9ba1c1db3c629530352be374794dec8c1a87f8ba
ManonYuki <manonyuki.rios@gmail.com>
2025-03-15 00:12:05 +0100

    j'ai fais les calculs mouahaha

---
+++ Manon_draft/Decomposed_calculations.py
@@ -0,0 +1,26 @@
+import math
+
+G = 6.67430e-11
+MTerre = 5.97219e24
+MLune = 7.3477e22
+
+positionT = [0,0,0]
+positionL = [3.844e8, 0, 0]
+
+# Distance
+dx = positionL[0] - positionT[0]
+dy = positionL[1] - positionT[1]
+dz = positionL[2] - positionT[2]
+
+# calcul de la distance entre les deux astres avec le théorème de Pythagore 3 dimensions
+r = math.sqrt(dx*dx + dy*dy + dz*dz)
+
+# print("La distance est de :", r) = 
+print(f"La distance est de : {r}")
+
+F = G * MTerre * MLune / r**2
+print(f"La force est de : {F} N") 
+
+# accélération Lune
+aL = F/MLune
+print(f"L'accélération de la Lune est de : {aL} m.s^-1")
